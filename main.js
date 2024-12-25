3e0a8dbd680385b98d06602e8d338aeb5cf43866
Егор <egor@MBP-Egor.netis.cc>
2024-12-25 17:34:30 +0100

    correct comments

--- main.js
+++ main.js
@@ -8,20 +8,14 @@ const swiper = new Swiper('.hero__swiper .swiper', {
     autoplay: {
         delay: 0,
     },
-  });
+});
   
-  const professions = document.querySelectorAll('.discover__profession')
-  console.log(professions);
   
-  const lists = document.querySelector(".discover__lists")
-  console.log(lists);
-  const buttons = document.querySelectorAll('.discover__list')
-  console.log(buttons);
-  const cards = document.querySelectorAll('.discover__cards')
-  console.log(cards);
-  
+const buttons = document.querySelectorAll('.discover__list')
+const cards = document.querySelectorAll('.discover__cards')
 
-  buttons.forEach(item => item.addEventListener('click', function(e){
+
+buttons.forEach(item => item.addEventListener('click', function(e){
     buttons.forEach((element) =>{
         element.classList.remove('list__active')
     })
@@ -41,66 +35,44 @@ const swiper = new Swiper('.hero__swiper .swiper', {
         console.log(data);
         document.querySelector(`.discover__content-${data}`).classList.remove('hidden')
     }
-   
-  }));
 
-
-// const sliderButtons = document.querySelector('.porfolios__buttons')
-// console.log(sliderButtons);
-// const prev = sliderButtons.firstElementChild
-// console.log(prev);
-// const next = sliderButtons.lastElementChild
-// console.log(next);
-// const slider = document.querySelector('.portfolio__cards')
-// const slide = document.querySelector('.portfolio__card')
-// const width = 461
-
-// next.addEventListener('click', function(e){
-//     slider.clientWidth += width
-   
-// })
-// prev.addEventListener('click', function(e){
-//     slider.clientWidth -= width
-// })
+}));
 
 const mySwiper = new Swiper(".mySwiper", {
     slidesPerView: 3,
     spaceBetween: 30,
     loop: true,
     keyboard: {
-      enabled: true,
+        enabled: true,
     },
     navigation: {
-      nextEl: ".next",
-      prevEl: ".prev",
+        nextEl: ".next",
+        prevEl: ".prev",
     },
-  });
+});
 
 
 
 
-  const saySwiper = new Swiper(".saySwiper", {
+const saySwiper = new Swiper(".saySwiper", {
     slidesPerView: 1,
     cssMode: true,
     loop: true,
     navigation: {
-      nextEl: ".button-next",
-      prevEl: ".button-prev",
+        nextEl: ".button-next",
+        prevEl: ".button-prev",
     },
     pagination: {
-      el: ".swiper-pagination",
+        el: ".swiper-pagination",
     },
     mousewheel: true,
     keyboard: true,
-  });
+});
 
 
 const Showanswer = document.querySelectorAll('.help__showanswer')
-console.log(Showanswer);
 const questionNumber = document.querySelectorAll(".help__question")
-console.log(questionNumber);
 const answerNumber = document.querySelectorAll('.help__answer')
-console.log(answerNumber);
 
 
 
